b692c7e6d4b099a47d9cd50443b86b0139ecaa67
ec2604 <erachoshen@gmail.com>
2018-02-24 12:36:33 +0200

    Basic code for searching for  parent directory

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # import_anywhere
-package which allows relative imports no matter where the script is run from
+This package allows relative imports no matter where the script is run from.
+
+This is to avoid situations when packages are used and relative imports only work when the script
+is run from its actual location. This package requires the user to list all "parent directories" which
+ the package is to  look for in the "IMPORT_ANYWHERE_DIRS" environment variable.
+
+An example of this problem
+If a script is located within a package with the following tree for example:
+
+ - MY_PARENT_FOLDER
+  - PACKAGE_A
+    -  C
+     - USEFUL_SCRIPT.PY
+    - MY_SCRIPT.PY
+
+And MY_SCRIPT contains the line "import C.USEFUL_SCRIPT.PY", this won't work if I'm running MY_SCRIPT.PY
+from a different directory.
+
+import_anywhere solves this problem if C (or PACKAGE_A) are located in the IMPORT_ANYWHERE_DIRS environment variable.
+
--- import_anywhere/import_anywhere.py
+++ import_anywhere/import_anywhere.py
@@ -1,8 +1,14 @@
-# the_imported.py
 import os
 import sys
 from inspect import currentframe
-top_directory_name = 'package_name'
+try:
+    if os.name == 'nt':
+        dir_names = os.environ['IMPORT_ANYWHERE_DIRS'].split(';')
+    else:
+        dir_names = os.environ['IMPORT_ANYWHERE_DIRS'].split(':')
+except KeyError:
+    print("Please set the environment variable IMPORT_ANYWHERE_DIRS", sys.stderr)
+    sys.exit(1)
 #Get frame for checking who imported
 frame = currentframe().f_back
 #Skip over irrelevant frames
@@ -11,7 +17,12 @@ while frame.f_code.co_filename.startswith('<frozen'):
 #current frame is file that imported package
 importer_filename = os.path.realpath(frame.f_code.co_filename)
 #Check where parent directory is located
-stop_idx = importer_filename.split(os.sep).index(top_directory_name)
-ancestor_path = os.sep.join(importer_filename.split(os.sep)[:stop_idx+1])
-#Append to sys path
-sys.path.append(ancestor_path)
+for dir_name in dir_names:
+    try:
+        stop_idx = importer_filename.split(os.sep).index(dir_name)
+        ancestor_path = os.sep.join(importer_filename.split(os.sep)[:stop_idx+1])
+        #Append to sys path
+        sys.path.append(ancestor_path)
+    except ValueError:
+        continue
+
