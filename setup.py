d4322c10b823b1ee9f930f78219e6aca957421ff
ec2604 <erachoshen@gmail.com>
2018-02-24 17:50:29 +0200

    Changed to version 1.2

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 setup(name='import_anywhere',
-      version='1.1',
+      version='1.2',
       description='Python Package allowing relative imports, no dependence on where python is run from',
-      long_description="Python Package allowing relative imports, no dependence on where python is run from. This allows python"
-                       " to run python files in a package, without running from the source folder. ",
+      long_description= "Python Package allowing relative imports, no dependence on where python is run from."
+                        " This allows python"
+                       " to run python files in a package, without running from the source folder.",
       author='Era Choshen',
       author_email='erachoshen@gmail.com',
       license='MIT',
       packages=['import_anywhere'],
       url= 'https://github.com/ec2604/import_anywhere',
-      download_url='https://github.com/ec2604/import_anywhere/archive/1.0.tar.gz',
+      download_url='https://github.com/ec2604/import_anywhere/archive/1.2.tar.gz',
       keywords=['relative_import'],
       python_requires = '>=3',
      )
